a3b2ce2dbc94248f18fb7c821ff07309b639b82e
MsMauve <zoetropesilver@gmail.com>
2025-04-04 21:53:14 -0600

    add draw function to grid boxes

--- etch_a_sketch.js
+++ etch_a_sketch.js
@@ -1,2 +1,16 @@
-const gridWrapper = document.querySelector("#grid-container")
-console.log(gridWrapper);
+const gridWrapper = document.querySelector("#grid-container");
+
+for (let i = 0; i < 16; i++) {
+    let gridBox = document.createElement("div");
+    gridBox.setAttribute("class", "grid-box");
+    // gridBox.setAttribute("id", `box-${i + 1}`);
+    gridWrapper.appendChild(gridBox);
+}
+
+const gridList = document.querySelectorAll(".grid-box");
+
+gridList.forEach((gridBox) => {
+    gridBox.addEventListener('mouseover', () => {
+        gridBox.style.backgroundColor = 'black'
+    });
+});
